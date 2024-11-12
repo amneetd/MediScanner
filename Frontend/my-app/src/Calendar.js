4d90beb700e702cd036e3480cb8d2907e9a49fc0
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2024-11-12 13:39:50 -0700

    added firebase config and connected front end login to firebase

--- Frontend/my-app/src/Calendar.js
+++ Frontend/my-app/src/Calendar.js
@@ -1,5 +1,5 @@
 // src/Calendar.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Calendar.css";
 
 const Calendar = () => {
--- Frontend/my-app/src/Login.js
+++ Frontend/my-app/src/Login.js
@@ -4,15 +4,36 @@ import { useDropzone } from 'react-dropzone';
 import './index.css';
 import { useNavigate } from 'react-router-dom'; 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import { collection, getDocs } from "firebase/firestore";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { db, auth } from "./Firebase-Configurations/firebaseConfig.js"
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const updateUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  }
 
   const nav = () => {
+    console.log(username, password);
+    signInWithEmailAndPassword(auth, username, password).then((userCredential) => {
+      console.log("signed in successfully");
+      const activeUser = userCredential.user;
+      navigate('/upload')
+    }).catch((error) => {
+      window.alert("trouble logging in");
+      console.log(error);
+    })
     navigate('/upload')
   }
-  const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -26,11 +47,12 @@ const LoginPage = () => {
       </header>
       <div className="login-box">
         <div className="login-back">
-            <input type="text" id="username" name="username" placeholder="Enter username" />
+            <input type="text" id="username" name="username" placeholder="Enter username" onChange={updateUsername}/>
             <input 
               type={showPassword ? 'text' : 'password'}  /* Toggle between password and text */
               name="password" 
-            placeholder="Enter password" />
+              placeholder="Enter password" 
+              onChange={updatePassword}/>
             <i 
               className={`fas ${showPassword ? 'fa-eye-slash' : 'fa-eye'} eye-icon`} 
               onClick={togglePasswordVisibility}
