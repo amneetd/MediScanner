f983fdaed2a54eab74181762d41ee4d6baced5fa
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2025-01-26 14:46:53 -0700

    added functions for saving and deleting medication

--- Frontend/my-app/src/Firebase-Configurations/firestore.js
+++ Frontend/my-app/src/Firebase-Configurations/firestore.js
@@ -9,6 +9,8 @@ import {
     doc,
     updateDoc,
     deleteField,
+    arrayUnion, 
+    arrayRemove
   } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword, updateProfile, updatePhoneNumber } from "firebase/auth";
 import { db } from "./firebaseConfig";
@@ -34,10 +36,7 @@ export const retrieveUserInformation = async (userUID) => {
 
 export const createNewDocument = async (userUID) => {
     await setDoc(doc(db, "users", userUID), {
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA",
-        savedMedications: ["hello"]
+        savedMedications: []
       });
 }
 
@@ -48,6 +47,7 @@ export const registerUser = async (username, email, phoneNumber, password) => {
       .then((userCredential) => {
         const user = userCredential.user;
         console.log("current User", auth.currentUser)
+        createNewDocument(user.uid)
         updateProfile(auth.currentUser, {
             displayName: username
         })
@@ -56,4 +56,36 @@ export const registerUser = async (username, email, phoneNumber, password) => {
       .catch((error) => {
         console.log(error)
       });
+}
+
+
+export const saveMedication = async (userUID) => {
+    const docRef = doc(db, "users", userUID);
+    await updateDoc(docRef, {
+        savedMedications: arrayUnion({
+            currentlyTaking: false,
+            dosage: 300,
+            endDate: "",
+            frequency: "",
+            medicationName: "",
+            startDate: ""
+        })
+    });
+}
+
+
+export const deleteMedication = async (userUID) => {
+    const docRef = doc(db, "users", userUID);
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap.data())
+    await updateDoc(docRef, {
+        savedMedications: arrayRemove({
+            currentlyTaking: false,
+            dosage: 300,
+            endDate: "",
+            frequency: "",
+            medicationName: "",
+            startDate: ""
+        })
+    });
 }
