acad109271b4f83eabb83987f103205e8ad5168d
T-o-mas <tomaskmet23@gmail.com>
2025-03-01 12:34:46 -0700

    added CORS

--- Scanner/trigger.py
+++ Scanner/trigger.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 import subprocess
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
 
 @app.route('/trigger', methods=['POST'])
 def trigger_photos():
     try:
         # Assuming 'clientPi.py' is the script that sends the photos.
         # subprocess.run executes the script. Adjust the command if needed.
-        subprocess.run(['python', 'clientPi.py'], check=True)
+        subprocess.run(['python', 'ClientPi.py'], check=True)
         return jsonify({'status': 'success', 'message': 'Photos sent.'}), 200
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
