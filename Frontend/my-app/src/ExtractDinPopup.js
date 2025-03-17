baab2314362d851bd7bc201117b7f60996b5dcf4
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2025-03-17 14:42:55 -0600

    added popup to display while DIN is being extracted from photo

---
+++ Frontend/my-app/src/ExtractDinPopup.js
@@ -0,0 +1,19 @@
+import React, { useCallback, useState, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
+import './ExtractDinPopup.css';
+import { useNavigate } from 'react-router-dom'; 
+import axios from 'axios';
+
+
+const ExtractDinPopup = () => {
+
+  return (
+    <div className='extract-wait-popup'>
+        <div className='extract-wait-container'>
+            <p>Please wait while your medication is identified. This can take a minute.</p>
+        </div>
+    </div>
+  );
+};
+
+export default ExtractDinPopup;
